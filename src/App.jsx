14c3c39c8eaa89aed7c887a3c3423e3f6492acd8
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2026-02-02 09:14:18 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -110,106 +110,126 @@ export default function App() {
   const LIMIT = 2500;
   const isExceeded = sanitizedMessage.length > LIMIT;
 
+  // Date/Time Functionality
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+      const timer = setInterval(() => {
+      setNow(new Date());
+      }, 1000);
+
+      return () => clearInterval(timer);
+  });
+
 
   return (
-    <div className="app-container">
-      <div className="card">
-        <div className="header">
-          <h1>Fiverr Message Sanitizer</h1>
-          <p className="subtitle">
-            Sanitize restricted keywords and translate safely to Bengali for Fiverr communication.
-          </p>
-
-          {/* Credit */}
-          <p className="subtitle">
-            Developed By <a href="https://bd.linkedin.com/in/ashfaque-hossain-abir-91151723b">Ashfaque Hossain Abir</a>
-          </p>
-        </div>
-
-        {/* Message Input */}
-        <div className="grid">
-          {/* LEFT COLUMN */}
-          <div className="column msg">
-            <div className="msg-col">
-              <div className="message-label-wrapper">
-
-                <label className="label">Your Message <span style={{ color: isExceeded ? "red" : "#000" }}>
-                      ({(sanitizedMessage.replace(/\s/g, '').length >= 1) ? sanitizedMessage.length : sanitizedMessage.replace(/\s/g, "").length} Characters)</span>
-                </label>
-
-                {message && (
-                  <button
-                    className="clear-btn"
-                    onClick={() => setMessage("")}
-                    aria-label="Clear input"
-                  >
-                    Clear
-                  </button>
-                )}
-              </div>
-
-              <textarea
-                className={`textarea ${hasRestricted ? "error" : ""}`}
-                placeholder="Type your message here..."
-                value={message}
-                onChange={ handleChange }
-              />
-            </div>
-
-            {hasRestricted && (
-              <div className="warning">
-                ⚠ Restricted content detected. Use the sanitized version.
-              </div>
-            )}
-          </div>
-
-          {/* RIGHT COLUMN */}
-          <div className="column">
-            <div className="preview-header">
-              <label className="label">Sanitized Preview</label>
-
-              <div className="actions">
-                <button
-                  className={`copy-btn ${copied ? "copied" : ""}`}
-                  onClick={handleCopy}
-                  disabled={!sanitizedMessage}
-                >
-                  {copied ? "✓ Copied" : "Copy"}
-                </button>
-
-                <button
-                  className="translate-btn"
-                  onClick={translateToBangla}
-                  disabled={!sanitizedMessage}
-                >
-                  🌐 Translate
-                </button>
-
-                <button className="fix-grammar-btn" onClick={fixGrammar} disabled={!sanitizedMessage}>
-                  <span className="icon">✓</span>
-                  <span className="text">Fix</span>
-                </button>
-              </div>
-            </div>
-
-            <div className="preview translate-area">
-              {(sanitizedMessage.replace(/\s/g, '').length >= 1) ? sanitizedMessage : "Nothing to preview yet."}
-            </div>
-
-
-            <button
-              className="save-btn"
-              onClick={() => saveAsTextFile(sanitizedMessage)}
-              disabled={!sanitizedMessage}
-            >
-              💾 Save as .txt
-            </button>
-
-          </div>
-        </div>
-
+    <>
+      {/* Time Component */}
+      <div className='time-comp'>
+        <p className='time-box'>Time: <span>{ now.toLocaleTimeString() }</span></p>
+        <p className='date-box'>Date: <span>{ now.toLocaleDateString() }</span></p>
       </div>
-    </div>
+
+      {/* App Component */}
+      <div className="app-container">
+        <div className="card">
+          <div className="header">
+            <h1>Fiverr Message Sanitizer</h1>
+            <p className="subtitle">
+              Sanitize restricted keywords and translate safely to Bengali for Fiverr communication.
+            </p>
+
+            {/* Credit */}
+            <p className="subtitle">
+              Developed By <a href="https://bd.linkedin.com/in/ashfaque-hossain-abir-91151723b">Ashfaque Hossain Abir</a>
+            </p>
+          </div>
+
+          {/* Message Input */}
+          <div className="grid">
+            {/* LEFT COLUMN */}
+            <div className="column msg">
+              <div className="msg-col">
+                <div className="message-label-wrapper">
+
+                  <label className="label">Your Message <span style={{ color: isExceeded ? "red" : "#000" }}>
+                        ({(sanitizedMessage.replace(/\s/g, '').length >= 1) ? sanitizedMessage.length : sanitizedMessage.replace(/\s/g, "").length} Characters)</span>
+                  </label>
+
+                  {message && (
+                    <button
+                      className="clear-btn"
+                      onClick={() => setMessage("")}
+                      aria-label="Clear input"
+                    >
+                      Clear
+                    </button>
+                  )}
+                </div>
+
+                <textarea
+                  className={`textarea ${hasRestricted ? "error" : ""}`}
+                  placeholder="Type your message here..."
+                  value={message}
+                  onChange={ handleChange }
+                />
+              </div>
+
+              {hasRestricted && (
+                <div className="warning">
+                  ⚠ Restricted content detected. Use the sanitized version.
+                </div>
+              )}
+            </div>
+
+            {/* RIGHT COLUMN */}
+            <div className="column">
+              <div className="preview-header">
+                <label className="label">Sanitized Preview</label>
+
+                <div className="actions">
+                  <button
+                    className={`copy-btn ${copied ? "copied" : ""}`}
+                    onClick={handleCopy}
+                    disabled={!sanitizedMessage}
+                  >
+                    {copied ? "✓ Copied" : "Copy"}
+                  </button>
+
+                  <button
+                    className="translate-btn"
+                    onClick={translateToBangla}
+                    disabled={!sanitizedMessage}
+                  >
+                    🌐 Translate
+                  </button>
+
+                  <button className="fix-grammar-btn" onClick={fixGrammar} disabled={!sanitizedMessage}>
+                    <span className="icon">✓</span>
+                    <span className="text">Fix</span>
+                  </button>
+                </div>
+              </div>
+
+              <div className="preview translate-area">
+                {(sanitizedMessage.replace(/\s/g, '').length >= 1) ? sanitizedMessage : "Nothing to preview yet."}
+              </div>
+
+
+              <button
+                className="save-btn"
+                onClick={() => saveAsTextFile(sanitizedMessage)}
+                disabled={!sanitizedMessage}
+              >
+                💾 Save as .txt
+              </button>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 
 }
